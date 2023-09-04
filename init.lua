9f0f63b5b7f8f03d4d6eff459caa6c704c61e64c
Rafael Bieze <rafael@methylium.com>
2023-09-04 09:53:13 +0200

    small changes

--- init.lua
+++ init.lua
@@ -60,7 +60,7 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
-
+  "ThePrimeagen/vim-be-good",
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
