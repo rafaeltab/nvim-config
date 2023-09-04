b86aaabc15fba881d97c83a57bd1a29a6d8b2c2f
Rafael Bieze <rafael@methylium.com>
2023-09-04 16:46:31 +0200

    Add treesitter dart

--- lua/base.lua
+++ lua/base.lua
@@ -207,7 +207,7 @@ function setup(plugins)
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'yaml',
-      'markdown' },
+      'markdown', 'dart' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
